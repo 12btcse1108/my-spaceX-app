a4b0ce3ae019813cd94e96db5d34bfed8e506014
nitin <nitinkumar.verma@in.imshealth.com>
2020-08-01 16:31:38 +0530

    ssr-working

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Home from "./Components/Home";
-import "./css/index.css";
 
 function App() {
   return (
--- webpack.server.js
+++ webpack.server.js
@@ -4,16 +4,25 @@ const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: "./server/index.js",
-  target: "node",
-  externals: [nodeExternals()],
 
   output: {
     path: path.resolve("server-build"),
     filename: "index.js",
   },
+  node: {
+    // Need this when working with express, otherwise the build fails
+    __dirname: false, // if you don't put this is, __dirname
+    __filename: false, // and __filename return blank or /
+  },
+  target: "node",
+  externals: [nodeExternals()],
 
   module: {
     rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
       {
         test: /\.js$/,
         use: "babel-loader",

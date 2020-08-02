3e70414ad21ac655b63a00b7e2ecb1ba799c2bb8
nitin <nitinkumar.verma@in.imshealth.com>
2020-08-02 17:11:58 +0530

    css changes2

--- src/Components/MissionsList.js
+++ src/Components/MissionsList.js
@@ -2,8 +2,7 @@ import React, { useEffect, useState } from "react";
 import MissionCard from "./MissionCard";
 
 function MissionList(props) {
-  const { initialData } = { ...props };
-  const [missionData, setMissionData] = useState(initialData);
+  const [missionData, setMissionData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [errors, setErrors] = useState("");
   const { filterValue } = { ...props };
@@ -11,14 +10,10 @@ function MissionList(props) {
     var queryString = Object.keys(filterValue)
       .map((key) => key + "=" + filterValue[key])
       .join("&");
-    console.log(queryString);
     const parse_url =
       Object.keys(filterValue).length > 0
         ? `https://api.spaceXdata.com/v3/launches?limit=100&${queryString}`
-        : ``;
-    console.log(parse_url);
-
-    // const url = `https://api.spaceXdata.com/v3/launches?limit=100&launch_success=${filterValue.launch_success}`;
+        : `https://api.spaceXdata.com/v3/launches?limit=100`;
     setIsLoading(true);
     fetch(parse_url)
       .then((res) => res.json())
